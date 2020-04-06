0d10438edc9192c513cda89cdaa64ffe7933127b
zhe <zhe@zhedeMac.local>
2020-04-05 23:12:20 -0400

    edge service

---
+++ user-edge-service/src/main/java/com/imooc/user/thrift/ServiceProvider.java
@@ -0,0 +1,66 @@
+package com.imooc.user.thrift;
+
+import com.imooc.thrift.message.MessageService;
+import com.imooc.thrift.user.UserService;
+import org.apache.thrift.TServiceClient;
+import org.apache.thrift.protocol.TBinaryProtocol;
+import org.apache.thrift.protocol.TProtocol;
+import org.apache.thrift.transport.*;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ServiceProvider {
+
+    @Value("${thrift.user.ip}")
+    private String serverIp;
+
+    @Value("${thrift.user.port}")
+    private int serverPort;
+
+    @Value("${thrift.message.ip}")
+    private String messageServerIp;
+
+    @Value("${thrift.message.port}")
+    private int messageServerPort;
+
+    private enum ServiceType {
+        USER,
+        MESSAGE
+    }
+
+    public UserService.Client getUserService() {
+
+        return getService(serverIp, serverPort, ServiceType.USER);
+    }
+
+    public MessageService.Client getMessasgeService() {
+
+        return getService(messageServerIp, messageServerPort, ServiceType.MESSAGE);
+    }
+
+    public <T> T getService(String ip, int port, ServiceType serviceType) {
+        TSocket socket = new TSocket(ip, port, 3000);
+        TTransport transport = new TFramedTransport(socket);
+        try {
+            transport.open();
+        } catch (TTransportException e) {
+            e.printStackTrace();
+            return null;
+        }
+
+        // client有公共父类 所以统一写返回值
+        TProtocol protocol = new TBinaryProtocol(transport);
+
+        TServiceClient result = null;
+        switch (serviceType) {
+            case USER:
+                result = new UserService.Client(protocol);
+                break;
+            case MESSAGE:
+                result = new MessageService.Client(protocol);
+                break;
+        }
+        return (T)result;
+    }
+}
